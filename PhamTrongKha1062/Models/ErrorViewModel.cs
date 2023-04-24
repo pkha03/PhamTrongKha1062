e0eec4c89484493c4ab7e45779a3357368f7766d
pkha03 <khap73854@gmail.com>
2023-04-24 15:14:35 +0700

    first commit

---
+++ PhamTrongKha1062/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PhamTrongKha1062.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
